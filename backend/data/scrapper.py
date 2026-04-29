3d3f61ca957ab4273e76c822556f99d074c84978
NightRzz <56657528+NightRzz@users.noreply.github.com>
2026-04-29 10:56:45 +0200

    Refactor database operations out of the scraper module and add type hints. Move data insertion logic to the database module, introduce a `setup_and_import` function for streamlined initialization, and enhance database error handling.

--- backend/data/scrapper.py
+++ backend/data/scrapper.py
@@ -1,7 +1,7 @@
 import asyncio
 import sqlite3
 from pathlib import Path
-
+from database import setup_and_import
 import aiohttp
 
 # Konfiguracja
@@ -9,7 +9,8 @@ MAX_CONCURRENT_REQUESTS = 4
 API_BASE = "https://www.themealdb.com/api/json/v1/1/"
 API_IMG = "https://www.themealdb.com/images/ingredients/"
 
-async def fetch_json(session, url):
+
+async def fetch_json(session: aiohttp.ClientSession, url: str) -> dict | None:
     """Uniwersalna funkcja do pobierania JSONa z API."""
     try:
         async with session.get(url, timeout=5) as response:
@@ -27,7 +28,7 @@ async def fetch_json(session, url):
         return None
 
 
-async def get_meal_ids(session):
+async def get_meal_ids(session: aiohttp.ClientSession) -> list[str]:
     """Pobiera unikalne ID wszystkich przepisów ze wszystkich kategorii."""
     print("Pobieram listę kategorii...")
     cat_data = await fetch_json(session, f"{API_BASE}list.php?c=list")
@@ -35,7 +36,7 @@ async def get_meal_ids(session):
 
     categories = [cat['strCategory'] for cat in cat_data['meals']]
 
-    # Pobieramy listy posiłków dla wszystkich kategorii współbieżnie
+    # Pobieramy listy id posiłków dla wszystkich kategorii konkurencyjnie
     tasks = [fetch_json(session, f"{API_BASE}filter.php?c={category}") for category in categories]
     results = await asyncio.gather(*tasks)
 
@@ -45,42 +46,47 @@ async def get_meal_ids(session):
             for m in res['meals']:
                 meal_ids.add(m['idMeal'])
 
-    print(f"Znaleziono {len(meal_ids)} unikalnych ID posiłków.")
+    print(f"Znaleziono {len(meal_ids)} ID posiłków.")
     return list(meal_ids)
 
 
-async def fetch_meal_details(session, meal_id, semaphore):
-    """Pobiera pełne dane o przepisie"""
+async def fetch_meal_details(
+        session: aiohttp.ClientSession,
+        meal_id: str,
+        semaphore: asyncio.Semaphore
+        ) -> dict | None:
+    """Pobiera pełne dane przepisu z API; Zwraca sformatowany słownik."""
+
     async with semaphore:
         url = f"{API_BASE}lookup.php?i={meal_id}"
         data = await fetch_json(session, url)
 
-        if not data or not data.get('meals'):
-            return None
+    if not data or not data.get('meals'):
+        return None
 
-        meal = data['meals'][0]
+    meal = data['meals'][0]
 
-        # ekstrakcja skladników i miar do listy par (składnik, miara)
-        ingredients = []
-        for i in range(1, 21):
-            ing = meal.get(f'strIngredient{i}')
-            meas = meal.get(f'strMeasure{i}')
-            if ing and ing.strip():
-                ingredients.append({
-                    "name": ing.strip().lower(),
-                    "measure": meas.strip() if meas else ""
-                })
+    # ekstrakcja skladników i miar do listy par (składnik, miara)
+    ingredients = []
+    for i in range(1, 21):
+        ing = meal.get(f'strIngredient{i}')
+        meas = meal.get(f'strMeasure{i}')
+        if ing and ing.strip():
+            ingredients.append({
+                "name": ing.strip().lower(),
+                "measure": meas.strip() if meas else ""
+            })
 
-        return {
-            "id": meal['idMeal'],
-            "title": meal['strMeal'],
-            "category": meal['strCategory'],
-            "area": meal['strArea'],
-            "instructions": meal['strInstructions'],
-            "thumbnail": meal['strMealThumb'],
-            "youtube":meal['strYoutube'],
-            "ingredients": ingredients
-        }
+    return {
+        "id": meal['idMeal'],
+        "title": meal['strMeal'],
+        "category": meal['strCategory'],
+        "area": meal['strArea'],
+        "instructions": meal['strInstructions'],
+        "thumbnail": meal['strMealThumb'],
+        "youtube":meal['strYoutube'],
+        "ingredients": ingredients
+    }
 
 
 async def main():
@@ -104,53 +110,13 @@ async def main():
                     print(f"Pobrano {len(final_meals)}/{len(meal_ids)}...")
 
     print("Zapisywanie danych do bazy SQLite...")
-    import_to_database(final_meals)
-    print("Gotowe! Dane są w bazie.")
-
-
-def get_or_create_id(cursor, table, name, img_url=None):
-    """Helper, zwraca ID rekordu, tworzy go jeśli nie istnieje."""
-    columns = "(name, image_url)" if img_url else "(name)"
-    params = [name, img_url] if img_url else [name]
-    cursor.execute(f"SELECT id FROM {table} WHERE name = ?", (name,))
-    row = cursor.fetchone()
-    if row:
-        return row[0]
-
-
-    cursor.execute(f"INSERT INTO {table} {columns}  VALUES ({'?'+ ',?' * bool(img_url)})", tuple(params))
-    return cursor.lastrowid
-
-def import_to_database(meals_list):
-    conn = sqlite3.connect(Path(__file__).parent / "empridge.db")
-    cursor = conn.cursor()
-
     try:
-        cursor.execute("PRAGMA foreign_keys = ON;")
-        cursor.execute("BEGIN TRANSACTION;")
-
-        for meal in meals_list:
-            category_id = get_or_create_id(cursor, "categories", meal['category'])
-            area_id = get_or_create_id(cursor, "areas", meal['area'])
-
-            cursor.execute('''
-                INSERT OR REPLACE INTO recipes (id, title, instructions, image_url, youtube_url, category_id, area_id)
-                VALUES (?, ?, ?, ?, ?, ?, ?)
-            ''', (meal['id'], meal['title'], meal['instructions'], meal['thumbnail'], meal['youtube'], category_id, area_id))
-
-            for ing in meal['ingredients']:
-                ing_id = get_or_create_id(cursor, "ingredients", ing['name'], f"{API_IMG}{ing['name']}.png")
-                cursor.execute('''
-                    INSERT OR REPLACE INTO recipe_ingredients (recipe_id, ingredient_id, measure)
-                    VALUES (?, ?, ?)
-                ''', (meal['id'], ing_id, ing['measure']))
-
-        conn.commit()
+        setup_and_import(final_meals, API_IMG)
     except Exception as e:
-        conn.rollback()
-        print(f"Błąd podczas zapisu: {e}")
-    finally:
-        conn.close()
+        print(f"Import nie powiódł się: {e}")
+    else:
+        print("Gotowe! Dane są w bazie.")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
